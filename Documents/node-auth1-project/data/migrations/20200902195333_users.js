852e5fb28fcf1f0564b07a8b7157c766ebecc764
Your Name <Reginaldbmiller@gmail.com>
2020-09-04 19:49:28 -0500

    update

--- Documents/node-auth1-project/data/migrations/20200902195333_users.js
+++ Documents/node-auth1-project/data/migrations/20200902195333_users.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('users', (table) =>{
+        table.increments()
+        table.text('username', 128).notNull().unique().index();
+        table.text('password', 256).notNull()
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('users')
 };
--- Documents/node-auth1-project/knexfile.js
+++ Documents/node-auth1-project/knexfile.js
@@ -12,7 +12,11 @@ module.exports = {
       directory: './data/migrations'
     }, 
     seeds: {
-      directory: '.data/seeds'
-    }
-  },
-};
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+  }};
